b924a733cc659553e3eb56296b9b0cba715a57cf
Saurav Barua <49154173+sauravbarua02@users.noreply.github.com>
2024-11-27 12:15:01 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const containerEl = document.getElementById("container");
+
+containerEl.innerText = `Branches of Civil Engineering are-
+Sturctural Engineering,
+Geotechnical Engineering
+Transportation Engineering
+Environmental Engineering
+Water resource Engineering`;
